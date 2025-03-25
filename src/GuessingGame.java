e85dfc49d23a5ab423e57626da4290db03f2a40f
angelomg10 <148798421+angelomg10@users.noreply.github.com>
2025-03-25 12:16:13 -0700

    Initial commit

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,10 +11,30 @@ import java.util.Scanner;
  */
 public class GuessingGame {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        do { playGame(scanner); } while (askToPlayAgain(scanner));
-        System.out.println("Thanks for playing!");
-        scanner.close();
+
+        //sdk user which game
+        String usersChoice;
+        Scanner input = new Scanner(System.in);
+
+        //call the loop
+        do {
+            System.out.println("type exit to stop, type 1 for rock paper scissors, type 2 to play guessing game");
+            usersChoice = input.nextLine();
+            if (Objects.equals(usersChoice, "1")) {
+                rpsGame();
+            } else if (Objects.equals(usersChoice, "2")) {
+
+                Scanner scanner = new Scanner(System.in);
+                playGame(scanner);
+
+            }
+
+        } while (!usersChoice.equalsIgnoreCase("exit"));
+
+//        Scanner scanner = new Scanner(System.in);
+//        do { playGame(scanner); } while (askToPlayAgain(scanner));
+//        System.out.println("Thanks for playing!");
+//        scanner.close();
     }
 
     private static void playGame(Scanner scanner) {
@@ -35,6 +56,73 @@ public class GuessingGame {
     private static boolean askToPlayAgain(Scanner scanner) {
         System.out.print("Play again? (Y/N): ");
         return scanner.next().trim().equalsIgnoreCase("Y");
+    }
+
+    public static void rpsGame(){
+
+
+        Scanner answer = new Scanner(System.in);
+        Random rand = new Random();
+        String result = " ";
+
+        System.out.println("Hello welcome, be ready!");
+        //maybe delete ***System.out.println("type 'exit' to stop program or type 1 to play rock paper scissors: ");
+
+
+
+        //run game
+
+        //create list for p2 answers
+        String[] numbers = new String[]{"rock", "paper", "scissors"};
+        //make player one (p1) choice
+        String p1;
+
+        //make player two (p2) choice
+        String p2;
+
+        System.out.println("type: 'rock', 'paper', or 'scissors' ");
+        p1 = answer.nextLine();
+
+
+        //creating p2 (bot) choice
+
+        p2 = numbers[rand.nextInt(2 - 0 + 1) + 0];
+
+        if (Objects.equals(p1, p2)) {
+            System.out.println("Players 1 choice: "+ p1);
+            System.out.println("Players 2 choice: "+ p2);
+            result = "tie";
+        } else if (Objects.equals(p1, "rock") && Objects.equals(p2, "scissors")) {
+            System.out.println("Players 1 choice: "+ p1);
+            System.out.println("Players 2 choice: "+ p2);
+            result = "You won by luck haha";
+        } else if (Objects.equals(p1, "scissors") && Objects.equals(p2, "paper")) {
+            System.out.println("Players 1 choice: "+ p1);
+            System.out.println("Players 2 choice: "+ p2);
+            result = "You won by luck haha";
+        } else if (Objects.equals(p1, "paper") && Objects.equals(p2, "rock")) {
+            System.out.println("Players 1 choice: "+ p1);
+            System.out.println("Players 2 choice: "+ p2);
+            result = "You won by luck haha";
+        } else if (Objects.equals(p1, "paper") && Objects.equals(p2, "scissors")) {
+            System.out.println("Players 1 choice: "+ p1);
+            System.out.println("Players 2 choice: "+ p2);
+            result = "You lost nub";
+        } else if (Objects.equals(p1, "scissors") && Objects.equals(p2, "rock")) {
+            System.out.println("Players 1 choice: "+ p1);
+            System.out.println("Players 2 choice: "+ p2);
+            result = "You lost nub";
+        } else if (Objects.equals(p1, "rock") && Objects.equals(p2, "paper")) {
+            System.out.println("Players 1 choice: "+ p1);
+            System.out.println("Players 2 choice: "+ p2);
+            result = "You lost nub";
+        }
+
+        System.out.println(result);
+
+
+
+
     }
 }
 
